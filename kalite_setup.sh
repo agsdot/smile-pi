98eb0e44ceb9c2c49e989feadd2a7bdf4c492120
Jonathan Lai <jonathanlai@gmail.com>
2016-09-12 16:41:10 -0700

    More kalite configurations

--- kalite_setup.sh
+++ kalite_setup.sh
@@ -7,23 +7,40 @@ echo ""
 rm -rf ~/.kalite/database/data.sqlite
 rm -rf ~/.kalite/secretkey.txt
 
+echo "pip install ka-lite"
 #http://stackoverflow.com/questions/29466663/memory-error-while-using-pip-install-matplotlib
 sudo /usr/bin/pip2 --no-cache-dir install ka-lite
 
 rm -rf ~/kalite_files
 mkdir -p ~/kalite_files
-cd ~/kalite_files
 
+echo "downloading english content pack for kalite"
+cd ~/kalite_files
 wget http://pantry.learningequality.org/downloads/ka-lite/0.16/content/contentpacks/en.zip
 
-wget http://pantry.learningequality.org/downloads/ka-lite/0.16/content/ka-lite-0.16-resized-videos-english.torrent
-
-sudo pacman -S aria2 --noconfirm --needed
+cd ~/kalite_files
 #(download file by torrenting it)
-#aria2c ka-lite-0.16-resized-videos-english.torrent --seed-time=0
+echo "downloading and torrenting a 38 gb file"
+echo "if at all possible try to seed/torrent this file from another nearby computer on the same subnet, it should expedite the process"
+echo "http://pantry.learningequality.org/downloads/ka-lite/0.16/content/ka-lite-0.16-resized-videos-english.torrent"
+wget http://pantry.learningequality.org/downloads/ka-lite/0.16/content/ka-lite-0.16-resized-videos-english.torrent
+sudo pacman -S aria2 --noconfirm --needed
+aria2c ka-lite-0.16-resized-videos-english.torrent --seed-time=0
 
+echo "kalite manage setup"
 /usr/bin/kalite manage setup --username=kalitedbadmin --password=kalitedbadmin123 --noinput
 
+echo "move videos in ~/.kalite/content directory"
+mv ~/.kalite/content/ ~/.kalite/content_backup/
+mv ~/kalite_files/ka-lite-0.16-resized-videos ~/.kalite/content/
+cp -r ~/.kalite/content_backup/assessement ~/.kalite/content/
+
+echo "kalite manage setup, run it again for safekeeping, new content was added to the ~/.kalite directory"
+# https://github.com/learningequality/ka-lite/issues/4991
+/usr/bin/kalite manage setup --username=kalitedbadmin --password=kalitedbadmin123 --noinput
+
+echo "/usr/bin/kalite manage retrievecontentpack local en ~/kalite_files/en.zip"
+echo "this may take awhile, as in 30 minutes plus or so..."
 /usr/bin/kalite manage retrievecontentpack local en ~/kalite_files/en.zip
 
 cd ~
