a32a87182875d972625883b58a381a23e063c0f2
Wilson Wang <wilsonohno@gmail.com>
2016-11-04 22:19:39 +0000

    add noconfirm on script

--- sensehat_setup.sh
+++ sensehat_setup.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 echo "sensehat python prep"
-
+cd /home/alarm/
 wget https://pypi.python.org/packages/15/be/b4fb144778db838aaf6bf86d939407627b2e6283503fa91195dbcafeb6c6/sense-hat-2.2.0.tar.gz#md5=69929250cb72349a8a82edf2584b1d83
 tar -zxvf sense-hat-2.2.0.tar.gz
+cd sense-hat-2.2.0
 sudo python2 setup.py install --root=$pkgdir/ --optimize=1
+
+echo "sensehat python packages, about 10 minutes"
 sudo /usr/bin/pip2.7 install -U numpy
 sudo /usr/bin/pip2.7 install -U pillow
 sudo /usr/bin/pip2.7 install -U pathlib
-
 
 echo "python library sensor prep"
 git clone https://github.com/RPi-Distro/RTIMULib
@@ -16,9 +18,10 @@ cd RTIMULib/Linux/python
 sudo /usr/bin/python2.7 setup.py build
 sudo /usr/bin/python2.7 setup.py install
 
-sudo pacman -Syu
-sudo pacman -S i2c-tools
-sudo pacman -S lm_sensors
+echo "get latest pacman and install sensors"
+sudo pacman -Syu --noconfirm --needed
+sudo pacman -S --noconfirm --needed i2c-tools
+sudo pacman -S --noconfirm --needed lm_sensors
 sudo cat 'dtparam=i2c_arm=on' >> /boot/config.txt
 sudo cat 'i2c-dev' >> /etc/modules-load.d/raspberrypi.conf
 sudo cat 'i2c-bcm2708' >> /etc/modules-load.d/raspberrypi.conf
