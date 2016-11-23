14e88ab5bd4b30d9ab91f4641108ed4a41607e93
Wilson Wang <wilsonohno@gmail.com>
2016-11-23 21:33:44 +0000

    display standard output instead of sending to /dev/null on couch script

--- setup_files/couch_setup.sh
+++ setup_files/couch_setup.sh
@@ -5,22 +5,22 @@
 
 echo "couch check"
 #if ( curl -X GET http://127.0.0.1:5984/smile 2>&1  | grep not_found ) then
-curl -X DELETE http://127.0.0.1:5984/smile 2>&1 > /dev/null
+curl -X DELETE http://127.0.0.1:5984/smile 2>&1
 echo "couch population"
-curl -X PUT http://127.0.0.1:5984/smile 2>&1 > /dev/null
+curl -X PUT http://127.0.0.1:5984/smile 2>&1
 
-curl -X PUT http://127.0.0.1:5984/smile/_design/activity --data-binary @./_design.activity.json 2>&1 > /dev/null
-curl -X PUT http://127.0.0.1:5984/smile/_design/generic --data-binary @./_design.generic.json 2>&1 > /dev/null
-curl -X PUT http://127.0.0.1:5984/smile/_design/group --data-binary @./_design.group.json 2>&1 > /dev/null
-curl -X PUT http://127.0.0.1:5984/smile/_design/institution --data-binary @./_design.institution.json 2>&1 > /dev/null
-curl -X PUT http://127.0.0.1:5984/smile/_design/resource --data-binary @./_design.resource.json 2>&1 > /dev/null
-curl -X PUT http://127.0.0.1:5984/smile/_design/response --data-binary @./_design.response.json 2>&1 > /dev/null
-curl -X PUT http://127.0.0.1:5984/smile/_design/session --data-binary @./_design.session.json 2>&1 > /dev/null
-curl -X PUT http://127.0.0.1:5984/smile/_design/user --data-binary @./_design.user.json 2>&1 > /dev/null
-curl -X PUT http://127.0.0.1:5984/smile/_design/message --data-binary @./_design.message.json 2>&1 > /dev/null
-curl -X PUT http://127.0.0.1:5984/smile/_design/usage --data-binary @./_design.usage.json 2>&1 > /dev/null
-curl -X PUT http://127.0.0.1:5984/smile/0eeeb110f2dcfb68d7bc63ede2002b44 --data-binary @./smileadmin.user.json 2>&1 > /dev/null
-curl -X PUT http://127.0.0.1:5984/smile/0eeeb110f2dcfb68d7bc63ede2002d47 --data-binary @./welcometosmile.group.json 2>&1 > /dev/null
+curl -X PUT http://127.0.0.1:5984/smile/_design/activity --data-binary @./_design.activity.json 2>&1
+curl -X PUT http://127.0.0.1:5984/smile/_design/generic --data-binary @./_design.generic.json 2>&1
+curl -X PUT http://127.0.0.1:5984/smile/_design/group --data-binary @./_design.group.json 2>&1
+curl -X PUT http://127.0.0.1:5984/smile/_design/institution --data-binary @./_design.institution.json 2>&1
+curl -X PUT http://127.0.0.1:5984/smile/_design/resource --data-binary @./_design.resource.json 2>&1
+curl -X PUT http://127.0.0.1:5984/smile/_design/response --data-binary @./_design.response.json 2>&1
+curl -X PUT http://127.0.0.1:5984/smile/_design/session --data-binary @./_design.session.json 2>&1
+curl -X PUT http://127.0.0.1:5984/smile/_design/user --data-binary @./_design.user.json 2>&1
+curl -X PUT http://127.0.0.1:5984/smile/_design/message --data-binary @./_design.message.json 2>&1
+curl -X PUT http://127.0.0.1:5984/smile/_design/usage --data-binary @./_design.usage.json 2>&1
+curl -X PUT http://127.0.0.1:5984/smile/0eeeb110f2dcfb68d7bc63ede2002b44 --data-binary @./smileadmin.user.json 2>&1
+curl -X PUT http://127.0.0.1:5984/smile/0eeeb110f2dcfb68d7bc63ede2002d47 --data-binary @./welcometosmile.group.json 2>&1
 
 #fi
 echo "done with couch script"
