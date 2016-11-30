4a9bcc9cac3d2761e135082ba8fd4efdd4a389f0
Wilson Wang <wilsonohno@gmail.com>
2016-11-30 22:10:21 +0000

    comment out elasticsearch river stuff

--- smile_setup.sh
+++ smile_setup.sh
@@ -10,8 +10,9 @@ $HOME/.node_modules/bin/npm install -g redis
 echo "install couchdb"
 sudo pacman -S --noconfirm --needed couchdb
 
-echo "install elasticsearch"
-sudo pacman -S --noconfirm --needed elasticsearch
+#echo "install elasticsearch"
+#sudo pacman -S --noconfirm --needed elasticsearch
+
 echo "clone the plug branch of the smile_v2 repo"
 
 if [ ! -d "~/smile_v2" ]; then
@@ -28,47 +29,47 @@ sed -i 's@/vagrant@'"$HOME"/smile_v2'@' /tmp/smile_backend.service
 sed -i 's@/usr/bin/node@'"$HOME"/.node_modules/bin/node'@' /tmp/smile_backend.service
 sudo cp /tmp/smile_backend.service /usr/lib/systemd/system/smile_backend.service
 
-echo "configure elasticsearch"
+#echo "configure elasticsearch"
 
-sudo mkdir -p /var/lib/elasticsearch
-sudo chown elasticsearch /var/lib/elasticsearch
-sudo chgrp elasticsearch /var/lib/elasticsearch
-sudo cp ~/smile_v2/vagrant/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+#sudo mkdir -p /var/lib/elasticsearch
+#sudo chown elasticsearch /var/lib/elasticsearch
+#sudo chgrp elasticsearch /var/lib/elasticsearch
+#sudo cp ~/smile_v2/vagrant/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 
 #sudo unzip analysis-icu*.zip -d analysis-icu/
 # cruft from doing a manual elasticsearch-plugin install, left there for documentation purposes
 
-cd /usr/share/elasticsearch/plugins/
-sudo rm -rf *
+#cd /usr/share/elasticsearch/plugins/
+#sudo rm -rf *
 
-echo "get elasticsearch marvel-agent plugin"
-echo y | sudo elasticsearch-plugin install marvel-agent
+#echo "get elasticsearch marvel-agent plugin"
+#echo y | sudo elasticsearch-plugin install marvel-agent
 
-echo "get elasticsearch-river-couchdb"
-cd /tmp
-sudo rm -rf *
+#echo "get elasticsearch-river-couchdb"
+#cd /tmp
+#sudo rm -rf *
 
-sudo elasticsearch-plugin install elasticsearch/elasticsearch-river-couchdb/2.6.0 > /dev/null
-sudo unzip elasticsearch-river-couchdb*.zip -d elasticsearch-river-couchdb
+#sudo elasticsearch-plugin install elasticsearch/elasticsearch-river-couchdb/2.6.0 > /dev/null
+#sudo unzip elasticsearch-river-couchdb*.zip -d elasticsearch-river-couchdb
 
-touch  /tmp/plugin-descriptor.properties
-echo "
-description=Elasticsearch River Couchdb
-version=2.6.0
-name=elasticsearch-river-couchdb
-site=false
-jvm=true
-classname=org.elasticsearch.plugin.river.couchdb.CouchdbRiverPlugin
-java.version=1.7
-elasticsearch.version=2.3.5
-isolated=false
-" >> /tmp/plugin-descriptor.properties
-sudo cp /tmp/plugin-descriptor.properties /tmp/elasticsearch-river-couchdb/plugin-descriptor.properties
-sudo mv /tmp/elasticsearch-river-couchdb /usr/share/elasticsearch/plugins/elasticsearch-river-couchdb
+#touch  /tmp/plugin-descriptor.properties
+#echo "
+#description=Elasticsearch River Couchdb
+#version=2.6.0
+#name=elasticsearch-river-couchdb
+#site=false
+#jvm=true
+#classname=org.elasticsearch.plugin.river.couchdb.CouchdbRiverPlugin
+#java.version=1.7
+#elasticsearch.version=2.3.5
+#isolated=false
+#" >> /tmp/plugin-descriptor.properties
+#sudo cp /tmp/plugin-descriptor.properties /tmp/elasticsearch-river-couchdb/plugin-descriptor.properties
+#sudo mv /tmp/elasticsearch-river-couchdb /usr/share/elasticsearch/plugins/elasticsearch-river-couchdb
 
-echo "cleanup tmp dir"
-cd /tmp
-sudo rm -rf *
+#echo "cleanup tmp dir"
+#cd /tmp
+#sudo rm -rf *
 
 echo "install ~/smile_v2/backend node dependencies"
 cd ~/smile_v2/backend/
@@ -100,9 +101,9 @@ echo "systemctl for couch"
 sudo systemctl enable couchdb
 sudo systemctl start couchdb
 
-echo "systemctl for elasticsearch"
-sudo systemctl enable elasticsearch
-sudo systemctl start elasticsearch
+#echo "systemctl for elasticsearch"
+#sudo systemctl enable elasticsearch
+#sudo systemctl start elasticsearch
 
 echo "systemctl for redis"
 sudo systemctl enable redis
