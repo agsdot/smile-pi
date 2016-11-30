da601f6c09c1ce3f3db139501539dbfdb7c506dc
Wilson Wang <wilsonohno@gmail.com>
2016-11-30 23:16:52 +0000

    add sensehat exit script

--- setup_files/sensehat-exit.py
+++ setup_files/sensehat-exit.py
@@ -1,2 +1,36 @@
 #!/usr/bin/python
-exit()
+
+from sense_hat import SenseHat
+from time import sleep
+import signal
+import sys
+
+w = [150, 150, 150]
+g = [0, 255, 0]
+r = [255, 0, 0]
+e = [0, 0, 0]
+          
+sad = [
+e,e,e,e,e,e,e,e,
+e,r,r,e,e,r,r,e,
+e,r,r,e,e,r,r,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,r,r,r,r,e,e,
+e,r,e,e,e,e,r,e,
+r,e,e,e,e,e,e,r
+]
+
+sense = SenseHat()
+
+sense.show_message("Shutdown", scroll_speed=(0.05), text_colour=(255, 0, 0), back_colour=(0, 0, 0))
+
+sense.show_letter("3")
+sleep(1)
+sense.show_letter("2")
+sleep(1)
+sense.show_letter("1")
+sleep(1)
+sense.set_pixels(sad)
+sys.exit(0)
+
