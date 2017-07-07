a900f78f866876f0029bdc06c8dc44966e85554b
Reuben Thiessen <rthiessen@edify.org>
2017-07-07 13:27:28 -0700

    adds usb to menu
    Signed-off-by: Reuben Thiessen <rthiessen@edify.org>

--- setup_scripts/automount_setup.sh
+++ setup_scripts/automount_setup.sh
@@ -38,4 +38,52 @@ sudo git clone https://github.com/luk1337/Directory-Theme.git /usr/share/nginx/h
 sudo \cp /etc/nginx/nginx.conf /etc/nginx/nginx_old.conf
 sudo \cp ~/smile-pi/setup_files/nginx.conf.rpi3 /etc/nginx/nginx.conf
 
+echo "update smile plug portal page"
+echo "clone the repo"
+git clone https://bitbucket.org/gseit/smile-plug-portal-web
+
+cd ~/smile-plug-portal-web/src/views/
+
+sed -i 's@/smile/frontend/src/@/smile/@' home.js
+#sed -i 's@:8001/@/wikipedia/@' home.js
+#sed -i 's@:8008/@/khan/@' home.js
+
+cd ~/smile-plug-portal-web/
+echo "build smile plug portal"
+
+LAST_FOUR_MAC_ADDRESS="$(ip addr | grep link/ether | awk '{print $2}' | tail -1  | sed s/://g | tr '[:lower:]' '[:upper:]' | tail -c 5)"
+
+cd ~/smile-pi/
+GIT_HASH="$(git rev-parse HEAD)"
+
+sudo sed -i 's@Administer@Administer Smile Plug (SMILE_'"$LAST_FOUR_MAC_ADDRESS"')@' ~/smile-plug-portal-web/src/templates/admin.html
+sudo sed -i 's@breadcrumb-->@breadcrumb '"$GIT_HASH"' -->@' ~/smile-plug-portal-web/src/templates/admin.html
+
+rm -f ~/smile-pi/home.js
+ruby ~/smile-pi/setup_files/build_portal.rb
+rm ~/smile-plug-portal-web/src/views/home.js
+cp ~/smile-pi/home.js ~/smile-plug-portal-web/src/views/home.js
+rm ~/smile-plug-portal-web/src/templates/navbar.html
+cp ~/smile-pi/setup_files/navbar_usb.html ~/smile-plug-portal-web/src/templates/navbar.html
+
+#jake build
+cd ~/smile-plug-portal-web/
+ jake build
+
+cd ~/smile-plug-portal-web/target/
+
+echo "put in place new smile plug portal files, html js assets"
+sudo rm -rf /usr/share/nginx/html/assets/
+sudo rm -rf /usr/share/nginx/html/js/
+sudo rm -rf /usr/share/nginx/html/index.html
+sudo rm -rf /usr/share/nginx/html/404.html
+sudo rm -rf /usr/share/nginx/html/50x.html
+
+
+sudo \cp -r ~/smile-plug-portal-web/target/assets/ /usr/share/nginx/html/assets/
+sudo \cp -r ~/smile-plug-portal-web/target/js/ /usr/share/nginx/html/js/
+sudo \cp -r ~/smile-plug-portal-web/target/index.html /usr/share/nginx/html/index.html
+sudo \cp ~/smile-pi/setup_files/404.html /usr/share/nginx/html/404.html
+sudo \cp ~/smile-pi/setup_files/50x.html /usr/share/nginx/html/50x.html
+
 sudo /etc/init.d/nginx restart
