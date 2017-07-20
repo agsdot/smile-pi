f5c8d50fbc2ddae13213223d1b15316d577169cf
Reuben Thiessen <rthiessen@edify.org>
2017-07-20 11:48:49 -0700

    forgot to save changes
    Signed-off-by: Reuben Thiessen <rthiessen@edify.org>

--- setup_scripts/update_ap.sh
+++ setup_scripts/update_ap.sh
@@ -4,7 +4,7 @@ echo "update SSID with LAST_FOUR_MAC_ADDRESS"
 
 LAST_FOUR_MAC_ADDRESS="$(ip addr | grep link/ether | awk '{print $2}' | tail -1  | sed s/://g | tr '[:lower:]' '[:upper:]' | tail -c 5)"
 
-sudo sed -i 's@Administer@Administer Smile Plug (SMILE_'"$LAST_FOUR_MAC_ADDRESS"')@' /usr/share/nginx/html/index.html
+sudo sed -i 's/SMILE_F69F/SMILE_'"$LAST_FOUR_MAC_ADDRESS"'/' /usr/share/nginx/html/index.html
 
 cd ~/smile-pi/setup_files/
 sudo rm -rf /usr/lib/systemd/system/create_ap.service
