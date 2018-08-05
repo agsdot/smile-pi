ee5eb50549d81d0265a76f7f3840e3174ba98347
Reuben Thiessen <rthiessen@edify.org>
2018-08-05 10:26:26 -0700

    Updates to predictable interface names for create_ap

--- setup_scripts/update_ap.sh
+++ setup_scripts/update_ap.sh
@@ -3,6 +3,7 @@
 echo "update SSID with LAST_FOUR_MAC_ADDRESS"
 
 LAST_FOUR_MAC_ADDRESS="$(ip addr | grep link/ether | awk '{print $2}' | tail -1  | sed s/://g | tr '[:lower:]' '[:upper:]' | tail -c 5)"
+ETH0_NAME="$(ip addr | grep 2: | awk '{print $2}' | tail -1  | sed s/://g)"
 
 echo "new SSID: $LAST_FOUR_MAC_ADDRESS"
 sudo sed -i 's/SMILE_F69F/SMILE_'"$LAST_FOUR_MAC_ADDRESS"'/' /usr/share/nginx/html/index.html
@@ -11,6 +12,7 @@ cd ~/smile-pi/setup_files/
 sudo rm -rf /usr/lib/systemd/system/create_ap.service
 sudo \cp -rf create_ap.service /usr/lib/systemd/system/create_ap.service
 sudo sed -i 's@ SMILE @ SMILE_'"$LAST_FOUR_MAC_ADDRESS"' @' /usr/lib/systemd/system/create_ap.service
+sudo sed -i 's@ eth0 @ '"$ETH0_NAME"' @' /usr/lib/systemd/system/create_ap.service
 
 echo "reload systemctl"
 sudo systemctl daemon-reload
